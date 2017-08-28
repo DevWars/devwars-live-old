641f58768df7ddf13e0d0e5568a9d5ca1b965038
Kim Simonsen <polydecay@users.noreply.github.com>
2017-08-28 15:30:24 +0200

    Add vue-router

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,20 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './components/app.vue';
 import './socket';
 import './styles/index.scss';
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: App },
+    ],
+});
+
 const app = new Vue({
     el: '#app',
-    render: h => h(App),
+    router,
+    render: h => h('router-view'),
 });
