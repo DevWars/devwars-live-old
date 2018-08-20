c6b944b552f202d930cad4e0e2a80638e7b0c443
Kim Simonsen <polydecay@users.noreply.github.com>
2018-08-20 12:35:51 +0200

    Refactor server initialization

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,38 @@
 const config = require('config');
-const server = require('./server');
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const helmet = require('helmet');
+const socketIo = require('socket.io');
+const Game = require('./game/Game');
+const routes = require('./routes');
+const socketAuth = require('./middleware/socketAuth');
 
-server.init();
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, { transports: ['websocket'] });
+io.use(socketAuth());
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+app.use(helmet({ frameguard: false }));
+app.use(express.static(path.resolve(__dirname, '../../public')));
+
+const game = new Game(io);
+
+app.use(routes);
+app.use('/game', game.router);
+app.use('*', (req, res) => res.sendStatus(404));
+
+app.use((err, req, res) => {
+    if (config.util.getEnv('NODE_ENV') === 'development') {
+        res.status(500).send(err.message);
+    } else {
+        res.sendStatus(500);
+    }
+});
 
 const port = config.get('port');
-server.server.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
